813925b9e610789e6acc0e3df0ab17b65af1a1db
PuteriDiyana97 <60600748+PuteriDiyana97@users.noreply.github.com>
2021-04-26 07:11:50 +0800

    Add files via upload

---
+++ application/models/voucher/Vouchers.php
@@ -0,0 +1,164 @@
+<?php
+class Vouchers extends CI_Model 
+{
+	public $table = 'data_voucher';
+	public $primary_key = 'id';
+
+	public function __construct()
+	{
+	}
+
+	function read_data($id)
+	{
+		$db = $this->db;
+		$db->select('*');
+		$db->where($this->primary_key,$id);
+		$db->limit(1);
+		$q = $db->get($this->table)->row();
+		return $q;
+	}
+
+	function insert($data)
+	{
+		$q = $this->db->insert($this->table, $data);
+        $insert_id = $this->db->insert_id();
+
+        return $insert_id;
+	}
+
+    function list_data()
+    {
+        $db = $this->db;
+        $db->select('*');
+        $db->where('active','1');
+        $q = $db->get($this->table)->row();
+        return $q;
+    }
+
+	function update_data($id, $data)
+	{
+		$db = $this->db;
+		$db->where($this->primary_key, $id);
+		$db->update($this->table, $data);
+		$q = $db->affected_rows();
+
+		return $q;
+	}
+
+	function delete_data($id)
+	{
+		$data = array(
+						"updated"=>getDateTime(),
+						"active"=>'0',
+					);
+
+		$db = $this->db;
+		$q = $db->where_in($this->primary_key, $id);
+		$q = $db->update($this->table, $data);
+
+		return $q;
+	}
+
+	// ======================= DATATABLE SERVER SIDE PROCESSING =========================
+
+    var $column_order = array(null, 'voucher_name', 'voucher_value', 'exchange_point', 'start_date', 'end_date', null); // field yang ada di table
+    var $column_search = array('voucher_name','start_date', 'end_date'); // field yang diizinkan untuk pencarian 
+    var $order = array('UPPER(voucher_name)' => 'ASC'); // default order
+
+    private function _get_datatables_query()
+    {
+    	$filter_name = $this->input->post('filter_name');
+        $start_date  = $this->input->post('filter_date_start');
+        $end_date    = $this->input->post('filter_date_end');
+
+    	$db = $this->db;
+    	$db->select('*');
+    	$db->from($this->table);
+    	$db->where('active', '1');
+         $db->where('voucher_type', '0');
+
+        
+        if(!empty($start_date))
+        {
+            $filter_date_start_temp = str_replace('/', '-', $start_date);
+
+            $db->where('DATE(start_date) >=',date("Y-m-d", strtotime($filter_date_start_temp)));
+        }
+         
+        if(!empty($end_date))
+        {
+            $filter_date_end_temp = str_replace('/', '-', $end_date);
+
+            $db->where('DATE(end_date) <=',date("Y-m-d", strtotime($filter_date_end_temp)));
+        }
+
+		if ( !empty($filter_name) ) 
+		{
+    		$db->like('voucher_name', $filter_name, 'both');
+		}
+
+		$i = 0;
+     
+        foreach ($this->column_search as $item) // looping awal
+        {
+            if ($_POST['search']['value'] ) // jika datatable mengirimkan pencarian dengan method POST
+            {
+                 
+                if ( $i===0 ) // looping awal
+                {
+                    $db->group_start(); 
+                    $db->like($item, $_POST['search']['value']);
+                }
+                else
+                {
+                    $db->or_like($item, $_POST['search']['value']);
+                }
+ 
+                if ( count($this->column_search) - 1 == $i ) 
+                    $db->group_end(); 
+            }
+            $i++;
+        }
+         
+        if ( isset($_POST['order']) ) 
+        {
+            $db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } 
+        else if ( isset($this->order) )
+        {
+            $order = $this->order;
+            $db->order_by(key($order), $order[key($order)]);
+        }
+    }
+
+    function get_datatables()
+    {
+    	$db = $this->db;
+        $this->_get_datatables_query();
+        if ( $_POST['length'] != -1 )
+        $db->limit($_POST['length'], $_POST['start']);
+        $query = $db->get();
+        // echo $db->last_query();
+        return $query->result();
+    }
+ 
+    function count_filtered()
+    {
+    	$db = $this->db;
+        $this->_get_datatables_query();
+        $query = $db->get();
+        return $query->num_rows();
+    }
+ 
+    public function count_all()
+    {
+    	$db = $this->db;
+        $db->from($this->table);
+        $db->where('active', '1');
+         $db->where('voucher_type', '0');
+        $c = $db->count_all_results();
+        // echo $db->last_query();
+        return $c;
+    }
+}
+?>
